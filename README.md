615d9c7e71a6bdcc61291189796127d314bfb695
Guilherme Sividal <guisividal@hotmail.com>
2021-04-20 21:31:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#
+
+##TECNOLOGIAS
+* JAVA 11
+* Spring Boot 2.4.x
+* Spring Data JPA
+* Postgres SQL 
+
+
+##RODANDO
+
+##COMO BUILDA
+
+https://gist.github.com/pedroits/9a42411f44ba9d75a70bfb7122c6f642
---
+++ src/test/java/br/com/gsividal/stoomproject/StoomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gsividal.stoomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
